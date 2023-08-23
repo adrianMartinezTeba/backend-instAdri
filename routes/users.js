31e0428caf3b9e9b34de949f035678256ada82b9
adrianMartinezTeba <adrymate@gmail.com>
2023-08-23 14:35:35 +0200

    añadido nodemailer y user controller

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const UserController = require("../controllers/UserController");
+const router = express.Router();
+const { authentication } = require("../middlewares/authentication")
+
+router.post("/register", UserController.register);
+router.get("/confirmRegister/:emailToken", UserController.confirm);
+router.post("/login", UserController.login);
+router.delete("/logout",authentication, UserController.logout);
+router.get("/userInfo", authentication,UserController.getInfoById);
+router.get('/recoverPassword/:email', UserController.recoverPassword);
+router.put("/resetPassword/:recoverToken", UserController.resetPassword);
+
+module.exports = router;
