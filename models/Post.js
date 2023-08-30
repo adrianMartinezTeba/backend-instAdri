b726d0ec3ac45500781cff9018c62a8b434d1b25
adrianMartinezTeba <adrymate@gmail.com>
2023-08-30 12:23:26 +0200

    registro funcionando en loclal

--- models/Post.js
+++ models/Post.js
@@ -3,13 +3,13 @@ const ObjectId = mongoose.SchemaTypes.ObjectId;
 
 const PostSchema = new mongoose.Schema(
   {
-    name: 
-      String,
-      // required: [true, "Por favor rellena tu nombre"],
-    
+    name:{  
+     type: String,
+      required: [true, "Por favor rellena tu nombre"]
+    },
     image: String,
     description: String,
-    // author: { type: ObjectId, ref: 'User', required: true }, // Nuevo campo para el autor del post
+    author: { type: ObjectId, ref: 'User', required: true }, // Nuevo campo para el autor del post
     likes: [{ type: ObjectId, ref: 'User' }]
   },
   { timestamps: true }
